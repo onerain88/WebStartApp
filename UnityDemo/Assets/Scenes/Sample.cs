b6a01cb6854ea4963523b2808711907ebea89845
oneRain <onerain88@gmail.com>
2022-04-01 18:23:58 +0800

    init

---
+++ UnityDemo/Assets/Scenes/Sample.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using RegistryLib;
+
+public class Sample : MonoBehaviour
+{
+    public Text exePathText;
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        string exePath = Process.GetCurrentProcess().MainModule.FileName;
+        
+        exePathText.text = exePath;
+        RegistryUtils.Register();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ WinLib/RegistryLib/RegistryUtils.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace RegistryLib
+{
+    public class RegistryUtils
+    {
+        public static void Register()
+        {
+            var classes = Registry.CurrentUser.OpenSubKey("Software", true).OpenSubKey("Classes", true);
+            // TODO Client Id
+            RegistryKey key = classes.CreateSubKey("wsapp");
+            // 
+            key.SetValue("URL Protocol", "code");
+            key.CreateSubKey(@"shell\open\command").SetValue("", $"{Process.GetCurrentProcess().MainModule.FileName} %1");
+        }
+    }
+}
